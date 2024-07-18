4e68ee9ce5ccf528297b3a024b5daabd6c17e1ac
saho-tine726 <sj_ej0726@outlook.jp>
2024-07-18 17:31:54 +0900

    完成

---
+++ src/app/api/post/[uuid]/route.ts
@@ -0,0 +1,69 @@
+import { NextResponse } from "next/server";
+import { doConnect, prisma } from "../route";
+
+// post詳細記事 取得API
+export const GET = async (req: Request, res: NextResponse) => {
+  try {
+    const uuid: string = req.url.split("/post/")[1];
+
+    await doConnect();
+
+    const post = await prisma.post.findFirst({
+      where: {
+        uuid,
+      },
+    });
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+// post詳細記事 編集API
+export const PUT = async (req: Request, res: NextResponse) => {
+  try {
+    const { title, content, authorId } = await req.json();
+
+    const uuid: string = req.url.split("/post/")[1];
+
+    await doConnect();
+
+    const post = await prisma.post.update({
+      data: {
+        title,
+        content,
+        authorId,
+      },
+      where: {
+        uuid,
+      },
+    });
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+// post詳細記事 削除API
+export const DELETE = async (req: Request, res: NextResponse) => {
+  try {
+    const uuid: string = req.url.split("/post/")[1];
+
+    await doConnect();
+
+    const post = await prisma.post.delete({
+      where: {
+        uuid,
+      },
+    });
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
